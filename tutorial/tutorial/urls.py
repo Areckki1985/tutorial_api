8def959e00bdbb51ed80954602ffc6470a7982b4
Arek <arkadiusz.gluchowicz@gmail.com>
2019-08-21 11:03:36 +0200

    URSL - final

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -15,28 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from snippets import views
-from django.conf.urls import include
 
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
 
-
+# The API URLs are now determined automatically by the router.
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    path('snippets/', views.SnippetList.as_view()),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
-
-    path('users/', views.UserList.as_view()),
-    path('users/<int:pk>/', views.UserDetail.as_view()),
-
-    path('api-auth/', include('rest_framework.urls')),
-
-    path('', views.api_root),
-
-    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view()),
-
+    path('', include(router.urls)),
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
